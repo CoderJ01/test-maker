26ea9dd708f45ba3cc766c42133aae8a0caa44b7
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-03 14:17:15 -0700

    require unique username and email per user

--- server/routes/User.js
+++ server/routes/User.js
@@ -8,8 +8,33 @@ const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
 router.post('/', async (req, res) => {
+    // hash
     const salt = await bcrypt.genSalt(10);
     const hashedPass = await bcrypt.hash(req.body.password, salt);
+
+    // find username 
+    const username = await User.findOne({
+        where: {
+            username: req.body.username
+        }
+    });
+
+    if(username) {
+        res.status(400).json({ msg: 'Username must be unique!' });
+        return;
+    }
+
+    // find email
+    const email = await User.findOne({
+        where: {
+            email: req.body.email
+        }
+    });
+
+    if(email) {
+        res.status(400).json({ msg: 'Email must be unique!' });
+        return;
+    }
 
     User.create({
         username: req.body.username,

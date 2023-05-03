e1ccec9012aaa449f739a32a7adcde939aef3ee4
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-02 19:24:25 -0700

    enable default value to be set for ids

--- server/models/Test.js
+++ server/models/Test.js
@@ -7,6 +7,7 @@ Test.init(
     {
         id: {
             type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
             allowNull: false,
             primaryKey: true
         },

5dc6eca319c9e798b66d7591dc313bf26c13cf7d
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-02 19:23:14 -0700

    create sequelize model for test

---
+++ server/models/Test.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Test extends Model {}
+
+Test.init(
+    {
+        id: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            primaryKey: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.UUID,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    }, 
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'test'
+    }
+);
+
+module.exports = Test;

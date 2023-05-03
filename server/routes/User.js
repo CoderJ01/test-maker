12e3f0b98d46bfadf333d118c9803bea702beb7f
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-03 14:05:12 -0700

    hash password

--- server/routes/User.js
+++ server/routes/User.js
@@ -1,11 +1,15 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
+
     User.create({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPass
     })
     .then(response => {
         res.json(response);

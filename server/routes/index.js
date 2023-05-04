8299e323fcf4283a28d95f320552e2c0a9d27664
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-03 17:14:19 -0700

    POST route for question

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,8 +4,10 @@ const router = require('express').Router();
 // routes
 const userRoutes = require('./user');
 const testRoutes = require('./test');
+const questionRoutes = require('./question');
 
 router.use('/users', userRoutes);
 router.use('/tests', testRoutes);
+router.use('/questions', questionRoutes);
 
 module.exports = router;
---
+++ server/routes/question.js
@@ -0,0 +1,24 @@
+// Express.js
+const router = require('express').Router();
+
+// model
+const Question = require('../models/Question');
+
+router.post('/:testId', async (req, res) => {
+    await Question.create({
+        question: req.body.question,
+        correct_answer: req.body.correct_answer,
+        second_choice: req.body.second_choice,
+        third_choice: req.body.third_choice,
+        fourth_choice: req.body.fourth_choice,
+        test_id: req.params.testId
+    })
+    .then(response => {
+        res.json(response)
+    })
+    .catch(error => {
+        res.json(error);
+    });
+});
+
+module.exports = router;

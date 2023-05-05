7a9a1f550acda912b891e54dc61ab5b0ca253b06
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-04 20:51:35 -0700

    create registration form

--- client/src/components/Register/Register.component.jsx
+++ client/src/components/Register/Register.component.jsx
@@ -1,10 +1,62 @@
 // React
-import React from 'react';
+import React, { useState } from 'react';
+
+// other imports
+import axios from 'axios';
 
 const Register = () => {
-    return (
-        <div className='register'>
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(username === '' || email === '' || password === '') {
+            alert('All fields need to be filled in!');
+            return;
+        }
+
+        if(password.length < 8) {
+            alert('Password needs to be at least 8 characters!');
+            return;
+        }
+
+        axios.post('http://localhost:3001/api/users/register', 
+            {
+                username: username,
+                email: email,
+                password: password
+            }
+        )
+        .then(response => {
+            console.log(response);
+        })
+        .catch(error => {
+            console.log(error);
+            alert(error.response.data.msg);
+        })
+    }
+
+    return (
+        <div className='register' onSubmit={handleSubmit}>
+            <form>
+                <div>
+                    <label htmlFor='username'>Username:</label><br/>
+                    <input type='text' name='username' value={username} onChange={(e) => setUsername(e.target.value)}/>
+                </div>
+                <div>
+                    <label htmlFor='email'>Email:</label><br/>
+                    <input type='text' name='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                </div>
+                <div>
+                    <label htmlFor='passowrd'>Password (8+ characters):</label><br/>
+                    <input type='password' name='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+                </div>
+                <button>
+                    Register
+                </button>
+            </form>
         </div>
     );
 }

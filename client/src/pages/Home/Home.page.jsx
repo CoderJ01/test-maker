730c3e1f584650570a12993205b49a51dc880310
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-04 19:16:13 -0700

    set up divisional structure of homepage

--- client/src/pages/Home/Home.page.jsx
+++ client/src/pages/Home/Home.page.jsx
@@ -2,7 +2,18 @@
 import React from 'react';
 
 const Home = () => {
-    return<>Home</>
+    return (
+        <div className='home'>
+            <div className='user-tests'></div>
+            <div>
+                <div className='tests'></div>
+                <div>
+                    <div className='scores'></div>
+                    <div className='logout'></div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Home;

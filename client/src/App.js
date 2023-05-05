fef13bca46b76daa66f1866cbf73455cdf3aea0a
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-04 17:03:48 -0700

    import Header to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // CSS
 import './App.css';
 
+// component
+import Header from './components/Header.component';
+
 function App() {
 	return (
 		<div className='App'>
 			<BrowserRouter>
 				<Routes>
-					<Route>
-						
+					<Route path='/' element={<Header/>}>
+
 					</Route>
 				</Routes>
 			</BrowserRouter>

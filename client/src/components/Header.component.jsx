8abbb4e1efa32d414c4ba159b83764b3d3ecfb33
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-04 17:26:28 -0700

    change Header jsx structure

--- client/src/components/Header.component.jsx
+++ client/src/components/Header.component.jsx
@@ -5,7 +5,11 @@ import React from 'react';
 import './Header.style.css';
 
 const Header = () => {
-    return<>Header</>
+    return (
+        <header>
+            <h1>Test Maker</h1>
+        </header>
+    );
 }
 
 export default Header;

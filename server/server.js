39946282781fb48fc966ee3ecd3812b62f964d34
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-05 17:08:07 -0700

    configure cors to allow cookies

--- server/server.js
+++ server/server.js
@@ -18,6 +18,8 @@ app.use(express.json());
 app.use(
     cors({
         origin: ['http://localhost:3000', process.env.DEPLOYED_FRONTEND],
+        methods:['POST', 'GET', 'PUT', 'DELETE'],
+        credentials: true
     })
 );
 app.use('/api', routes);

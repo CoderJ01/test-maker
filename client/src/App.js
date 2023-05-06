64870e3eea5160d50be40291836d0c66476f9429
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-05 17:08:52 -0700

    import Login component into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import './App.css';
 // component
 import Header from './components/Header/Header.component';
 import Register from './components/Register/Register.component';
+import Login from './components/Login/Login.component';
 
 // pages
 import Home from './pages/Home/Home.page';
@@ -20,6 +21,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/register' element={<Register/>}/>
+          <Route path='/login' element={<Login/>}/>
         </Routes>
       </BrowserRouter>
     </div>

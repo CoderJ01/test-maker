2e18d52d54a72fe3a8e6c3e28dfc9a05b90aac8e
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-05 18:39:14 -0700

    clean

--- client/src/pages/Home/Home.page.jsx
+++ client/src/pages/Home/Home.page.jsx
@@ -9,15 +9,8 @@ import UserTests from '../../components/UserTests/UserTests.component';
 import Tests from '../../components/Tests/Tests.component';
 import Scores from '../../components/Scores/Scores.component';
 import Logout from '../../components/Logout/Logout.component';
-import Register from '../../components/Register/Register.component';
 
 const Home = ({ user }) => {
-
-    if(user.length === 0 ) {
-       return (
-            <Register/>
-       );
-    }
 
     return (
         <div className='home'>

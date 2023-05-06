770daff0a880975fe247f75c6382cd05e75e00e8
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-05 21:06:02 -0700

    jsx structure for Tests component

--- client/src/components/Tests/Tests.component.jsx
+++ client/src/components/Tests/Tests.component.jsx
@@ -8,6 +8,16 @@ const Tests = () => {
     return (
         <div className='tests'>
             <h2>Tests</h2>
+            <div className='test-single-test'>
+                <h3>Title</h3>
+                <p>Commande apercoit laissons bas mon essaiera ame campagne parterre. 
+                    Fils on idee mais la me qu deux vive sons. Vif bourse une hordes 
+                    demain voyait regard poemes.</p>
+                <div className='tst-footer'>
+                    <p>Questions: 25</p>
+                    <p>Created by: user</p>
+                </div>
+            </div>
         </div>
     );
 }

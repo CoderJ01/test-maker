f4d711744f0418f1cc44620bd45248c9f23bab14
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-05 17:28:45 -0700

    fetch logged in user

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 // React
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // CSS
 import './App.css';
 
-// component
+// components
 import Header from './components/Header/Header.component';
 import Register from './components/Register/Register.component';
 import Login from './components/Login/Login.component';
@@ -13,7 +13,35 @@ import Login from './components/Login/Login.component';
 // pages
 import Home from './pages/Home/Home.page';
 
+// util
+import { baseURL } from './utils/urls';
+
+// other imports
+import axios from 'axios';
+import cookie from 'js-cookie';
+
 function App() {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    const fetchUser = async() => {
+      try {
+        let userCookie = cookie.get('test-maker-cookie');
+        const response = await axios.get(`${baseURL}/api/users`);
+        for(let i = 0; i < response.data.length; i++) {
+          if(response.data[i].random_string === userCookie) {
+            setUser(response.data[i]);
+          }
+        }
+      }
+      catch(error) {
+        console.log(error);
+      }
+    }
+    fetchUser();
+  }, []);
+  console.log(user);
+  
   return (
     <div className='App'>
       <Header/>

95e33ccec5c125ca52f58d26aeafbb2d556c6030
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-05 19:46:17 -0700

    logout button functionality

--- client/src/components/Logout/Logout.component.jsx
+++ client/src/components/Logout/Logout.component.jsx
@@ -4,10 +4,20 @@ import React from 'react';
 // CSS 
 import './Logout.style.css';
 
+// other imports
+import cookie from 'js-cookie';
+
 const Logout = () => {
+    
+    const handleLogout = (event) => {
+        event.preventDefault();
+        cookie.remove('test-maker-cookie');
+        window.location.reload(false);
+    }
+
     return (
         <div className='logout'>
-            Logout
+            <button onClick={handleLogout}>Logout</button>
         </div>
     );
 }

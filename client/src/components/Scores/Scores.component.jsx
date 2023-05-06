5c103f2106d2d6357795cd80383a405a9a5a8562
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-06 13:16:07 -0700

    create Score component divisions

--- client/src/components/Scores/Scores.component.jsx
+++ client/src/components/Scores/Scores.component.jsx
@@ -8,6 +8,10 @@ const Scores = () => {
     return (
         <div className='scores'>
             <h2>Scores</h2>
+            <div className='scores-single-score'>
+                <p><span style={{ fontWeight: 'bold' }}>Test: </span>Test Name</p>
+                <p><span style={{ fontWeight: 'bold' }}>Score: </span>80</p>
+            </div>
         </div>
     );
 }

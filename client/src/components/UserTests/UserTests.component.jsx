1f24f66f1b6847f52ebd41cb62c327321833de9b
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-05 20:30:18 -0700

    render sample data for user's tests

--- client/src/components/UserTests/UserTests.component.jsx
+++ client/src/components/UserTests/UserTests.component.jsx
@@ -8,6 +8,38 @@ const UserTests = () => {
     return (
         <div className='user-tests'>
             <h2>Your Tests</h2>
+            <div className='user-test-display'>
+                <div className='utd-single-test'>
+                    <h3>Title</h3>
+                    <p>Commande apercoit laissons bas mon essaiera ame campagne parterre. 
+                    Fils on idee mais la me qu deux vive sons. Vif bourse une hordes demain voyait regard poemes. 
+                   </p>
+                </div>
+                <div className='utd-single-test'>
+                    <h3>Title</h3>
+                    <p>Commande apercoit laissons bas mon essaiera ame campagne parterre. 
+                    Fils on idee mais la me qu deux vive sons. Vif bourse une hordes demain voyait regard poemes. 
+                   </p>
+                </div>
+                <div className='utd-single-test'>
+                    <h3>Title</h3>
+                    <p>Commande apercoit laissons bas mon essaiera ame campagne parterre. 
+                    Fils on idee mais la me qu deux vive sons. Vif bourse une hordes demain voyait regard poemes. 
+                   </p>
+                </div>
+                <div className='utd-single-test'>
+                    <h3>Title</h3>
+                    <p>Commande apercoit laissons bas mon essaiera ame campagne parterre. 
+                    Fils on idee mais la me qu deux vive sons. Vif bourse une hordes demain voyait regard poemes. 
+                   </p>
+                </div>
+                <div className='utd-single-test'>
+                    <h3>Title</h3>
+                    <p>Commande apercoit laissons bas mon essaiera ame campagne parterre. 
+                    Fils on idee mais la me qu deux vive sons. Vif bourse une hordes demain voyait regard poemes. 
+                   </p>
+                </div>
+            </div>
         </div>
     );
 }

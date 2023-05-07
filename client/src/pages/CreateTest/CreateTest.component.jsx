ebb13c5527eb7f3edaa630bbabe941ffbc153856
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-06 17:11:28 -0700

    allow user to set the number of questions

--- client/src/pages/CreateTest/CreateTest.component.jsx
+++ client/src/pages/CreateTest/CreateTest.component.jsx
@@ -1,7 +1,12 @@
 // React
 import React from 'react';
 
+// util
+import { listOptions } from './CreateTest.util';
+
 const CreateTest = () => {
+
+    const options = listOptions();
     
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -18,6 +23,19 @@ const CreateTest = () => {
                 <div>
                     <label htmlFor='description'>Description:</label><br/>
                     <textarea type='text' name='description'></textarea>
+                </div>
+                <div>
+                    <label htmlFor='questions'>Number of Questions:</label><br/>
+                    <select 
+                        // value={selected} 
+                        // onChange={e => setSelected(e.target.value)}
+                    >
+                    {options.map((value) => (
+                    <option value={value} key={value}>
+                        {value}
+                    </option>
+                    ))}
+                </select>
                 </div>
                 <button type='submit'>Confirm</button>
             </form>

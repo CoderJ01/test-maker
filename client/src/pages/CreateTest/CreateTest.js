3cc9381e410cf6a8a4125ea118b4384dee2253e8
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-06 17:00:53 -0700

    test creation form

--- client/src/pages/CreateTest/CreateTest.js
+++ client/src/pages/CreateTest/CreateTest.js
@@ -2,8 +2,26 @@
 import React from 'react';
 
 const CreateTest = () => {
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+    
     return (
-        <>Create Test</>
+        <div className='create-test'>
+            <form onSubmit={handleSubmit}>
+                <h2>Create Test</h2>
+                <div>
+                    <label htmlFor='title'>Title:</label><br/>
+                    <input type='text' name='title'></input>
+                </div>
+                <div>
+                    <label htmlFor='description'>Description:</label><br/>
+                    <textarea type='text' name='description'></textarea>
+                </div>
+                <button type='submit'>Confirm</button>
+            </form>
+        </div>
     );
 }
 

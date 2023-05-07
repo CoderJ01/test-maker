08bed99e1b4cf2a8966451bca29579bbea551fb1
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-07 15:04:28 -0700

    selection bar with test names

--- client/src/pages/CreateQuestions/CreateQuestions.page.jsx
+++ client/src/pages/CreateQuestions/CreateQuestions.page.jsx
@@ -1,8 +1,87 @@
 // React 
-import React from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+// url
+import { baseURL } from '../../utils/urls';
+
+// other imports
+import axios from 'axios';
 
 const CreateQuestions = () => {
-    return<>Questions</>
+    const { userId } = useParams();
+
+    const [tests, setTests] = useState([]);
+    const [testTitles, setTestTitles] = useState([]);
+    const [pickedTest, setPickedTest] = useState('');
+    const [testId, setTestId] = useState('');
+
+    const fetchTest = useCallback(async () => {
+        const id = userId;
+
+        if(id) {
+            try {
+                const response = await axios.get(`${baseURL}/api/tests/${id}`);
+                setTests(response.data);
+                setPickedTest(response.data[0].title)
+            }
+            catch(error) {
+                console.log(error);
+            }
+        }
+    }, [userId, setTests]);
+
+    useEffect(() => {
+        fetchTest();
+    }, [fetchTest]);
+
+    const retrieveNames =  useCallback(() => {
+        let incompleteTests = [];
+        tests.filter(test => {
+            if(test.complete === false) {
+                incompleteTests.push(test.title);
+            }
+            return test;
+        });
+        setTestTitles(incompleteTests);
+    }, [tests]);
+
+    useEffect(() => {
+        retrieveNames();
+    }, [retrieveNames]);
+
+    const retrieveIds = useCallback(() => {
+        tests.filter(test => {
+            if(pickedTest === test.title) {
+                setTestId(test.id);
+            }
+            return test;
+        });
+    }, [tests, pickedTest, setTestId]);
+
+    useEffect(() => {
+        retrieveIds();
+    }, [retrieveIds]);
+    
+    console.log(testId);
+
+    return (
+        <div className='create-questions'>
+            <h2>Create Questions</h2>
+            <div className='cq-selection'>
+                <select 
+                    value={pickedTest} 
+                    onChange={e => setPickedTest(e.target.value)}
+                >
+                    {testTitles.map((value) => (
+                    <option value={value} key={value}>
+                        {value}
+                    </option>
+                    ))}
+                </select>
+            </div>
+        </div>
+    );
 }
 
 export default CreateQuestions;

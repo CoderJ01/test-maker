3e8aae79aa58a5ef9a956de047bf960409d60b10
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-06 17:26:09 -0700

    useState variables

--- client/src/pages/CreateTest/CreateTest.component.jsx
+++ client/src/pages/CreateTest/CreateTest.component.jsx
@@ -1,16 +1,23 @@
 // React
-import React from 'react';
+import React, { useState } from 'react';
 
 // util
 import { listOptions } from './CreateTest.util';
 
 const CreateTest = () => {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [selected, setSelected] = useState('10');
 
     const options = listOptions();
     
     const handleSubmit = (e) => {
         e.preventDefault();
     }
+
+    console.log(title);
+    console.log(description);
+    console.log(selected)
     
     return (
         <div className='create-test'>
@@ -18,17 +25,17 @@ const CreateTest = () => {
                 <h2>Create Test</h2>
                 <div>
                     <label htmlFor='title'>Title:</label><br/>
-                    <input type='text' name='title'></input>
+                    <input type='text' name='title' onChange={e => setTitle(e.target.value)}></input>
                 </div>
                 <div>
                     <label htmlFor='description'>Description:</label><br/>
-                    <textarea type='text' name='description'></textarea>
+                    <textarea type='text' name='description' onChange={e => setDescription(e.target.value)}></textarea>
                 </div>
                 <div>
                     <label htmlFor='questions'>Number of Questions:</label><br/>
                     <select 
-                        // value={selected} 
-                        // onChange={e => setSelected(e.target.value)}
+                        value={selected} 
+                        onChange={e => setSelected(e.target.value)}
                     >
                     {options.map((value) => (
                     <option value={value} key={value}>

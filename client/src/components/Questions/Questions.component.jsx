d6c7b2106061753d0840941a43f1ea0c55dd9321
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-07 19:16:38 -0700

    add outer division to Questions component

--- client/src/components/Questions/Questions.component.jsx
+++ client/src/components/Questions/Questions.component.jsx
@@ -13,34 +13,44 @@ const Questions = () => {
     }
 
     return (
-        <div className='cqq-single-question'>
-            <br/>
-            <div>
-                <label htmlFor='question' id='cqqsq-heading' style={{ fontWeight: 'bold' }}>Question</label><br/>
-                <input type='text' name='question' onChange={e => setQuestion(e.target.value)}></input>
-            </div>
-            <br/>
-            <div>
-                <label htmlFor='correct-answer'>Correct Answer</label><br/>
-                <input type='text' name='correct-answer' onChange={e => setCorrect(e.target.value)}></input>
-            </div>
-            <br/>
-            <div>
-                <label htmlFor='second-choice'>Choice</label><br/>
-                <input type='text' name='second-choice' onChange={e => setChoice2(e.target.value)}></input>
-            </div>
-            <br/>
-            <div>
-                <label htmlFor='third-choice'>Choice</label><br/>
-                <input type='text' name='third-choice' onChange={e => setChoice3(e.target.value)}></input>
-            </div>
-            <br/>
-            <div>
-                <label htmlFor='fourth-choice'>Choice</label><br/>
-                <input type='text' name='fourth-choice' onChange={e => setChoice4(e.target.value)}></input>
-            </div>
-            <br/>
-            <button onClick={addQuestion}>Add</button>
+        <div className='cq-questions'>
+            <form>
+            {
+                Array.apply(0, Array(1)).map(function(x, i) {
+                    return (
+                        <div className='cqq-single-question'>
+                            <br/>
+                            <div>
+                                <label htmlFor='question' id='cqqsq-heading' style={{ fontWeight: 'bold' }}>Question</label><br/>
+                                <input type='text' name='question' onChange={e => setQuestion(e.target.value)}></input>
+                            </div>
+                            <br/>
+                            <div>
+                                <label htmlFor='correct-answer'>Correct Answer</label><br/>
+                                <input type='text' name='correct-answer' onChange={e => setCorrect(e.target.value)}></input>
+                            </div>
+                            <br/>
+                            <div>
+                                <label htmlFor='second-choice'>Choice</label><br/>
+                                <input type='text' name='second-choice' onChange={e => setChoice2(e.target.value)}></input>
+                            </div>
+                            <br/>
+                            <div>
+                                <label htmlFor='third-choice'>Choice</label><br/>
+                                <input type='text' name='third-choice' onChange={e => setChoice3(e.target.value)}></input>
+                            </div>
+                            <br/>
+                            <div>
+                                <label htmlFor='fourth-choice'>Choice</label><br/>
+                                <input type='text' name='fourth-choice' onChange={e => setChoice4(e.target.value)}></input>
+                            </div>
+                            <br/>
+                            <button onClick={addQuestion}>Add</button>
+                        </div>
+                    );
+                })
+            }
+            </form>
         </div>
     );
 }

8bf5c33e1a7db8ee4e9fb08818893d466b0bab48
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-07 19:09:20 -0700

    import

--- client/src/pages/CreateQuestions/CreateQuestions.page.jsx
+++ client/src/pages/CreateQuestions/CreateQuestions.page.jsx
@@ -5,6 +5,9 @@ import { useParams } from 'react-router-dom';
 // CSS
 import './CreateQuestions.style.css';
 
+// components
+import Questions from '../../components/Questions/Questions.component';
+
 // url
 import { baseURL } from '../../utils/urls';
 
@@ -110,35 +113,7 @@ const CreateQuestions = () => {
                 {
                     Array.apply(0, Array(1)).map(function(x, i) {
                         return (
-                            <div className='cqq-single-question'>
-                                <br/>
-                                <div>
-                                    <label htmlFor='question' id='cqqsq-heading' style={{ fontWeight: 'bold' }}>Question {i + 1}</label><br/>
-                                    <input type='text' name='question' onChange={e => setQuestion(e.target.value)}></input>
-                                </div>
-                                <br/>
-                                <div>
-                                    <label htmlFor='correct-answer'>Correct Answer</label><br/>
-                                    <input type='text' name='correct-answer' onChange={e => setCorrect(e.target.value)}></input>
-                                </div>
-                                <br/>
-                                <div>
-                                    <label htmlFor='second-choice'>Choice</label><br/>
-                                    <input type='text' name='second-choice' onChange={e => setChoice2(e.target.value)}></input>
-                                </div>
-                                <br/>
-                                <div>
-                                    <label htmlFor='third-choice'>Choice</label><br/>
-                                    <input type='text' name='third-choice' onChange={e => setChoice3(e.target.value)}></input>
-                                </div>
-                                <br/>
-                                <div>
-                                    <label htmlFor='fourth-choice'>Choice</label><br/>
-                                    <input type='text' name='fourth-choice' onChange={e => setChoice4(e.target.value)}></input>
-                                </div>
-                                <br/>
-                                <button onClick={addQuestion}>Add</button>
-                            </div>
+                            <Questions/>
                         );
                     })
                 }

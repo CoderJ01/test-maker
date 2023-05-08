9ea9f7f1d2c5b6b59d474b18ed82f9e00b386b3e
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-07 19:59:59 -0700

    remove jsx Array loop

--- client/src/components/Questions/Questions.component.jsx
+++ client/src/components/Questions/Questions.component.jsx
@@ -23,7 +23,7 @@ const Questions = ({ testId, questionNumber }) => {
             choice3: choice3,
             choice4: choice4
         };
-        
+
         setQuestions([...questions, questionObj]);
     }
 
@@ -32,41 +32,35 @@ const Questions = ({ testId, questionNumber }) => {
     return (
         <div className='questions'>
             <form>
-            {
-                Array.apply(0, Array(1)).map(function(x, i) {
-                    return (
-                        <div className='question'>
-                            <br/>
-                            <div>
-                                <label htmlFor='question' id='question-heading' style={{ fontWeight: 'bold' }}>Question</label><br/>
-                                <input type='text' name='question' onChange={e => setQuestion(e.target.value)}></input>
-                            </div>
-                            <br/>
-                            <div>
-                                <label htmlFor='correct-answer'>Correct Answer</label><br/>
-                                <input type='text' name='correct-answer' onChange={e => setCorrect(e.target.value)}></input>
-                            </div>
-                            <br/>
-                            <div>
-                                <label htmlFor='second-choice'>Choice</label><br/>
-                                <input type='text' name='second-choice' onChange={e => setChoice2(e.target.value)}></input>
-                            </div>
-                            <br/>
-                            <div>
-                                <label htmlFor='third-choice'>Choice</label><br/>
-                                <input type='text' name='third-choice' onChange={e => setChoice3(e.target.value)}></input>
-                            </div>
-                            <br/>
-                            <div>
-                                <label htmlFor='fourth-choice'>Choice</label><br/>
-                                <input type='text' name='fourth-choice' onChange={e => setChoice4(e.target.value)}></input>
-                            </div>
-                            <br/>
-                            <button onClick={addQuestion}>Add</button>
-                        </div>
-                    );
-                })
-            }
+                <div className='question'>
+                    <br/>
+                    <div>
+                        <label htmlFor='question' id='question-heading' style={{ fontWeight: 'bold' }}>Question</label><br/>
+                        <input type='text' name='question' onChange={e => setQuestion(e.target.value)}></input>
+                    </div>
+                    <br/>
+                    <div>
+                        <label htmlFor='correct-answer'>Correct Answer</label><br/>
+                        <input type='text' name='correct-answer' onChange={e => setCorrect(e.target.value)}></input>
+                    </div>
+                    <br/>
+                    <div>
+                        <label htmlFor='second-choice'>Choice</label><br/>
+                        <input type='text' name='second-choice' onChange={e => setChoice2(e.target.value)}></input>
+                    </div>
+                    <br/>
+                    <div>
+                        <label htmlFor='third-choice'>Choice</label><br/>
+                        <input type='text' name='third-choice' onChange={e => setChoice3(e.target.value)}></input>
+                    </div>
+                    <br/>
+                    <div>
+                        <label htmlFor='fourth-choice'>Choice</label><br/>
+                        <input type='text' name='fourth-choice' onChange={e => setChoice4(e.target.value)}></input>
+                    </div>
+                    <br/>
+                    <button onClick={addQuestion}>Add</button>
+                </div>
             </form>
         </div>
     );

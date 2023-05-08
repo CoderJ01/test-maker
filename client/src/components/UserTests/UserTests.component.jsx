481ceed77e38b37eb248d7b8805a7ed90caaa319
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-08 15:29:50 -0700

    replace dummy data with user tests

--- client/src/components/UserTests/UserTests.component.jsx
+++ client/src/components/UserTests/UserTests.component.jsx
@@ -1,11 +1,38 @@
 // React
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 // CSS
 import './UserTests.style.css';
 
+// URL
+import { baseURL } from '../../utils/urls';
+
+// other imports
+import axios from 'axios';
+
 const UserTests = ({ user }) => {
+    const [tests, setTests] = useState([]);
+
+    const fetchTests = useCallback(async () => {
+        const id = user.id;
+
+        if(id) {
+            try {
+                const response = await axios.get(`${baseURL}/api/tests/all/${id}`);
+                console.log(response.data);
+                setTests(response.data);
+            }
+            catch(error) {
+                console.log(error);
+            }
+        }
+    }, [user.id]);
+
+    useEffect(() => {
+        fetchTests();
+    }, [fetchTests]);
+
     let navigate = useNavigate();
 
     const routeChange = () => {
@@ -16,14 +43,39 @@ const UserTests = ({ user }) => {
     return (
         <div className='user-tests'>
             <h2>Your Tests</h2>
-            <div className='user-test-display'>
-                <div className='utd-single-test'>
-                    <h3>Title</h3>
-                    <p>Commande apercoit laissons bas mon essaiera ame campagne parterre. 
-                    Fils on idee mais la me qu deux vive sons. Vif bourse une hordes demain voyait regard poemes. 
-                   </p>
-                </div>
-            </div>
+            {
+                tests.length === 0 ? 
+                (
+                    <div className='ut-no-test'>
+                        <br/>
+                        <p> You have not created a test yet!</p>
+                        <br/>
+                    </div>
+                ) :
+                (
+                    <div className='user-test-display'>
+                    {
+                        tests.map(test => {
+                            return (
+                                <div className='utd-single-test'>
+                                    <h3>{test.title}</h3>
+                                    <p>{test.description}</p>
+                                    {
+                                        test.complete === true ? 
+                                        (
+                                            ''
+                                        ) : 
+                                        (
+                                            <p id='utdst-incomplete'>Incomplete</p>
+                                        )
+                                    }
+                                </div>
+                            );
+                        })
+                    }
+                    </div>
+                )
+            }
             <div className='ut-bottom'>
                 <button onClick={routeChange}>Create New Test</button>
             </div>

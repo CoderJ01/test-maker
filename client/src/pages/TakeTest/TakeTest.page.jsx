9a07f764ef606456947845ae2e1e26dc5bacce36
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-09 14:09:57 -0700

    jsx adjustment

--- client/src/pages/TakeTest/TakeTest.page.jsx
+++ client/src/pages/TakeTest/TakeTest.page.jsx
@@ -10,27 +10,35 @@ const TakeTest = ({ user }) => {
     return (
         <div className='take-test'>
             <h2>Test Name</h2>
-            <p>test description</p>
+            <br/>
+            <p style={{ textAlign: 'center' }}>test description</p>
             <div className='take-test-test'>
                 <form>
-                    <p>Question</p>
+                    <h3>Question</h3>
+                    <br/>
                     <div>
                         <input type='radio' name='test' onChange={e => setPickedChoice('A')}></input>
                         <label>choice1</label>
                     </div>
+                    <br/>
                     <div>
                         <input type='radio' name='test' onChange={e => setPickedChoice('B')}></input>
                         <label>choice2</label>
                     </div>
+                    <br/>
                     <div>
                         <input type='radio' name='test' onChange={e => setPickedChoice('C')}></input>
                         <label>choice3</label>
                     </div>
+                    <br/>
                     <div>
                         <input type='radio' name='test' onChange={e => setPickedChoice('D')}></input>
                         <label>choice4</label>
                     </div>
-                    <button>Submit Answer</button>
+                    <br/>
+                    <div className='ttt-submit-answer'>
+                        <button>Submit Answer</button>
+                    </div>
                 </form>
             </div>
         </div>

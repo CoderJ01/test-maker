80b9baed455c16d10030341481ecbb1cebedc8d6
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-08 20:59:32 -0700

    render all tests

--- client/src/components/Tests/Tests.component.jsx
+++ client/src/components/Tests/Tests.component.jsx
@@ -1,23 +1,61 @@
 // React
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // CSS
 import './Tests.style.css';
 
-const Tests = () => {
+// URL
+import { baseURL } from '../../utils/urls';
+
+// other imports
+import axios from 'axios';
+
+const Tests = ({ user }) => {
+    const [tests, setTests] = useState([]);
+
+    useEffect(() => {
+        const fetchTests = async () => {
+            try {
+                const response = await axios.get(`${baseURL}/api/tests`);
+                console.log(response.data);
+                setTests(response.data);
+            }
+            catch(error) {
+                console.log(error);
+            }
+        }
+        fetchTests();
+    }, []);
+
     return (
         <div className='tests'>
             <h2>Tests</h2>
-            <div className='test-single-test'>
-                <h3>Title</h3>
-                <p>Commande apercoit laissons bas mon essaiera ame campagne parterre. 
-                    Fils on idee mais la me qu deux vive sons. Vif bourse une hordes 
-                    demain voyait regard poemes.</p>
-                <div className='tst-footer'>
-                    <p>Questions: 25</p>
-                    <p>Created by: user</p>
-                </div>
-            </div>
+            {
+                tests.length === 0 ? 
+                (
+                    <>
+                    <br/>
+                    <p style={{ textAlign: 'center' }}>No other users have created a test yet!</p>
+                    </>
+                ) :
+                (
+                    tests.map(test => {
+                        return (
+                            <div className='test-single-test'>
+                                <h3>{test.title}</h3>
+                                <p>{test.description}</p>
+                                <div className='tst-footer'>
+                                    <p>Questions: {test.number_of_questions}</p>
+                                    <p>Created by: user</p>
+                                </div>
+                            </div>
+                        );
+                    })
+                )
+            }
+            {
+
+            }
         </div>
     );
 }

ba3b6175a812845ce9a1cbc818c0d3623ce0c7d0
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-09 13:37:53 -0700

    TakeTest jsx divisions

--- client/src/pages/TakeTest/TakeTest.page.jsx
+++ client/src/pages/TakeTest/TakeTest.page.jsx
@@ -1,8 +1,37 @@
 // React 
-import React from 'react';
+import React, { useState } from 'react';
 
-const TakeTest = () => {
-    return<>Take Test</>
+const TakeTest = ({ user }) => {
+    const [pickedChoice, setPickedChoice] = useState('');
+    console.log(pickedChoice);
+    return (
+        <div className='take-test'>
+            <h2>Test Name</h2>
+            <p>test description</p>
+            <div className='take-test-test'>
+                <form>
+                    <p>Question</p>
+                    <div>
+                        <input type='radio' name='test' onChange={e => setPickedChoice('A')}></input>
+                        <label>choice1</label>
+                    </div>
+                    <div>
+                        <input type='radio' name='test' onChange={e => setPickedChoice('B')}></input>
+                        <label>choice2</label>
+                    </div>
+                    <div>
+                        <input type='radio' name='test' onChange={e => setPickedChoice('C')}></input>
+                        <label>choice3</label>
+                    </div>
+                    <div>
+                        <input type='radio' name='test' onChange={e => setPickedChoice('D')}></input>
+                        <label>choice4</label>
+                    </div>
+                    <button>Submit Answer</button>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default TakeTest;

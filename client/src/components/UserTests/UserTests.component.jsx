84bd3a686a206a82b1cbd27618ebfe6eabb5a853
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-08 17:21:47 -0700

    modify route

--- client/src/components/UserTests/UserTests.component.jsx
+++ client/src/components/UserTests/UserTests.component.jsx
@@ -58,7 +58,7 @@ const UserTests = ({ user }) => {
                         tests.map(test => {
                             return (
                                 <div className='utd-single-test'>
-                                    <a href={`${baseURL_client}/viewtest/${user.id}/${test.id}`} target='_blank' rel='noopener noreferrer'><h3>{test.title}</h3></a>
+                                    <a href={`${baseURL_client}/view-test/${user.id}/${test.id}`} target='_blank' rel='noopener noreferrer'><h3>{test.title}</h3></a>
                                     <p>{test.description}</p>
                                     {
                                         test.complete === true ? 

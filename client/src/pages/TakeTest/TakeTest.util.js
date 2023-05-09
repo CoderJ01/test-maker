3061eada202bc6ed7f3e4fc43ee9deb2715ffc31
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-09 15:57:06 -0700

    create util functions

---
+++ client/src/pages/TakeTest/TakeTest.util.js
@@ -0,0 +1,33 @@
+export const modifyQuestions = (questions) => {
+    let modifiedQuestions = [];
+
+    // put answer choices per question in the same array
+    for(let i = 0; i < questions.length; i++) {
+        modifiedQuestions[i] = {
+            question: questions[i].question_header,
+            choices: [
+                questions[i].correct_answer,
+                questions[i].second_choice,
+                questions[i].third_choice,
+                questions[i].fourth_choice
+            ]
+        }
+    }
+
+    return modifiedQuestions;
+}
+
+export const shuffleAnswerChoices = (array) => {
+    let currentIndex = array.length,  randomIndex;
+
+    while (currentIndex !== 0) {
+
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [array[currentIndex], array[randomIndex]] = 
+        [array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}

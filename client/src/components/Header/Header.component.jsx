c1ce2aa496c41f1dc97d66435e9d86f29fb85b38
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-08 18:49:17 -0700

    convert site heading into link

--- client/src/components/Header/Header.component.jsx
+++ client/src/components/Header/Header.component.jsx
@@ -4,10 +4,13 @@ import React from 'react';
 // CSS
 import './Header.style.css';
 
+// URL
+import { baseURL_client } from '../../utils/urls';
+
 const Header = ({ user }) => {
     return (
         <header>
-            <h1>Test Maker</h1>
+            <h1><a href={`${baseURL_client}`}>Test Maker</a></h1>
             {
                 user.length === 0 ? 
                 (

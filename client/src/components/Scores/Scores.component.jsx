8e2afa7842635b45ceb0375c22df081771f68c00
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-09 20:45:38 -0700

    replace dummy data with score

--- client/src/components/Scores/Scores.component.jsx
+++ client/src/components/Scores/Scores.component.jsx
@@ -1,17 +1,50 @@
 // React
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 // CSS
 import './Scores.style.css';
 
-const Scores = () => {
+// URL
+import { baseURL } from '../../utils/urls';
+
+// other imports
+import axios from 'axios';
+
+const Scores = ({ user }) => {
+    const [scores, setScores] = useState([]);
+
+    const fetchScores = useCallback(async () => {
+        const id = user.id;
+
+        if(id) {
+            try {
+                const response = await axios.get(`${baseURL}/api/scores/${id}`);
+                console.log(response.data);
+                setScores(response.data);
+            }
+            catch(error) {
+                console.log(error);
+            }
+        }
+    }, [user.id]);
+
+    useEffect(() => {
+        fetchScores();
+    }, [fetchScores]);
+
     return (
         <div className='scores'>
             <h2>Scores</h2>
-            <div className='scores-single-score'>
-                <p><span style={{ fontWeight: 'bold' }}>Test: </span>Test Name</p>
-                <p><span style={{ fontWeight: 'bold' }}>Score: </span>80</p>
-            </div>
+            {
+                scores.map(score => {
+                    return (
+                        <div className='scores-single-score'>
+                            <p><span style={{ fontWeight: 'bold' }}>Test: </span>Test Name</p>
+                            <p><span style={{ fontWeight: 'bold' }}>Score: </span>{score?.score}</p>
+                        </div>
+                    );
+                })
+            }
         </div>
     );
 }

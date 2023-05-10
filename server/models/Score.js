755d9c9745af816ac72f260c5383f8f1b3f12853
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-09 18:13:26 -0700

    create Sequelize model Score

---
+++ server/models/Score.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Score extends Model {}
+
+Score.init(
+    {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true
+        },
+        score: {
+            type: DataTypes.DECIMAL,
+            allowNull: false
+        },
+        test_id: {
+            type: DataTypes.UUID,
+            references: {
+                model: 'test',
+                key: 'id'
+            }
+        },
+        user_id: {
+            type: DataTypes.UUID,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+module.export = Score;

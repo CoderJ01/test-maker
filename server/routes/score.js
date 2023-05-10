02487d825973a39770d4b0ebd7f44924bb2d8705
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-09 19:55:08 -0700

    keep track of user's score

--- server/routes/score.js
+++ server/routes/score.js
@@ -3,9 +3,36 @@ const router = require('express').Router();
 
 // model
 const Score = require('../models/Score');
+const Question = require('../models/Question');
 
 router.get('/', (req, res) => {
     res.send('Hello World');
+});
+
+router.post('/:testId/:userId', async (req, res) => {
+    const questions = await Question.findAll({
+        where: {
+            test_id: req.params.testId
+        }
+    });
+
+    let count = 0;
+    for(let i = 0; i < questions.length; i++) {
+        if(req.body.answers[i] === questions[i].dataValues.correct_answer) {
+            count++;
+        }
+    }
+
+    const score = await Score.create({
+        score: count/questions.length,
+        test_id: req.params.testId,
+        user_id: req.params.userId
+    });
+
+    res.send({
+        msg: `The score is ${score.score}!`,
+        data: score
+    });
 });
 
 module.exports = router;

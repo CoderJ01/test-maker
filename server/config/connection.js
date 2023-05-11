1159e65a34fb40a00c75fafc10bd715010358de0
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-10 18:38:24 -0700

    connect to ClearDB database

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,21 +1,23 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(
-    process.env.DATABASE,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD,
-    {
-        host: 'localhost',
-        dialect: 'mysql'
-    }
-);
+const sequelize = new Sequelize(process.env.CLEAR_DB_DATABASE);
 
-sequelize.authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(error => {
-        console.log('Unable to connect to the database: ', error);
-    });
+// const sequelize = new Sequelize(
+//     process.env.DATABASE,
+//     process.env.DB_USERNAME,
+//     process.env.DB_PASSWORD,
+//     {
+//         host: 'localhost',
+//         dialect: 'mysql'
+//     }
+// );
+
+// sequelize.authenticate()
+//     .then(() => {
+//         console.log('Connection has been established successfully.');
+//     })
+//     .catch(error => {
+//         console.log('Unable to connect to the database: ', error);
+//     });
 
 module.exports = sequelize;

fb6eb25682044ba4189e8b59edd14ccd4ee1e1ba
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-11 16:50:37 -0700

    create function to send verification emails

---
+++ server/util/validateEmail.js
@@ -0,0 +1,52 @@
+// Node.js packages
+const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+
+const validateEmail = (username, email, reason) => {
+    let message;
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
+        }
+    });
+      
+    const token = jwt.sign({
+            data: 'Token Data'  
+        }, process.env.SECRET_KEY, { expiresIn: '10m' }  
+    ); 
+
+    // The route ${process.env.REGISTRATION}/testmaker.auth/verify/${email}/${token} is located in another GitHub repository for security reasons
+    if(reason === 'newUser') {
+        message = `Hi ${username}! There, You have recently visited 
+        Test Maker and entered your email.
+        Please follow the given link to verify your email
+        ${process.env.REGISTRATION}/testmaker/auth/verify/${email}/${token} 
+        Thanks!`
+    }
+      
+    const mailConfigurations = {
+        from: 'joshua001testmaker@gmail.com',
+        to: email,
+        subject: 'Email Verification for Test Maker',
+        text: message
+    };
+      
+    transporter.sendMail(mailConfigurations, function(error, info){
+        if (error) {
+            console.log(error);
+            throw Error(error);
+        }
+        else {
+            console.log('Email Sent Successfully');
+            console.log(info);
+        }
+    });
+}
+
+module.exports = validateEmail;

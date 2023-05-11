e8530d13f36aaf96d77e04d94675e8fbb35b691d
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-10 18:38:47 -0700

    clean

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,22 +2,4 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.CLEAR_DB_DATABASE);
 
-// const sequelize = new Sequelize(
-//     process.env.DATABASE,
-//     process.env.DB_USERNAME,
-//     process.env.DB_PASSWORD,
-//     {
-//         host: 'localhost',
-//         dialect: 'mysql'
-//     }
-// );
-
-// sequelize.authenticate()
-//     .then(() => {
-//         console.log('Connection has been established successfully.');
-//     })
-//     .catch(error => {
-//         console.log('Unable to connect to the database: ', error);
-//     });
-
 module.exports = sequelize;

a642ed6b24f1dc18c601a446b4597d21943fc71a
CoderJ01 <buildingguy@sbcglobal.net>
2023-11-10 16:42:30 -0800

    create AccessDenied component

---
+++ client/src/components/AccessDenied/AccessDenied.component.jsx
@@ -0,0 +1,15 @@
+// React
+import React from 'react';
+
+// CSS
+import './AccessDenied.style.css';
+
+const AccessDenied = () => {
+    return (
+        <div className='access-denied'>
+            <text> Access denied! Sign up or log in for access to this page!</text>
+        </div>
+    );
+}
+
+export default AccessDenied;

31803ba132495676e4b7c6d533455370446117a1
CoderJ01 <buildingguy@sbcglobal.net>
2023-11-13 18:53:21 -0800

    dynamic display of number of questions

---
+++ client/src/components/NumberOfQuestions/NumberOfQuestions.jsx
@@ -0,0 +1,38 @@
+// React
+import React, { useState, useEffect, useCallback } from 'react';
+
+// URL
+import { baseURL } from '../../utils/urls';
+
+// other imports
+import axios from 'axios';
+
+const NumberOfQuestions = ({ testId }) => {
+    const [questions, setQuestions] = useState();
+
+    const fetchQuestions = useCallback(async () => {
+        const id = testId;
+
+        if(id) {
+            if(id) {
+                try {
+                    const response = await axios.get(`${baseURL}/api/questions/${id}`);
+                    setQuestions(response.data);
+                }
+                catch(error) {
+                    console.log(error);
+                }
+            }
+        }
+    }, [testId]);
+
+    useEffect(() => {
+        fetchQuestions();
+    }, [fetchQuestions]);
+
+    return (
+        <p id='tst-questions'>Number of Questions: {questions?.length}</p>
+    )
+}
+
+export default NumberOfQuestions;

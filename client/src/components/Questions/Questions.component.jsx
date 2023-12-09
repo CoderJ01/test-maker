4d5c0554ae011e886b67a8488276571173978c53
CoderJ01 <buildingguy@sbcglobal.net>
2023-12-09 15:11:36 -0800

    from modifications

--- client/src/components/Questions/Questions.component.jsx
+++ client/src/components/Questions/Questions.component.jsx
@@ -6,91 +6,21 @@ import { useNavigate } from 'react-router-dom';
 import './Questions.style.css';
 
 // utils
-import { setTextFieldsBlank } from './Questions.util';
-
-// URL
-import { baseURL } from '../../utils/urls';
-
-// other imports
-import axios from 'axios';
+import { processQuestion, processSubmission } from './Questions.util';
 
 const Questions = ({ user, testId, numOfQuestions }) => {
-    const [question, setQuestion] = useState('');
-    const [correct, setCorrect] = useState('');
-    const [choice2, setChoice2] = useState('');
-    const [choice3, setChoice3] = useState('');
-    const [choice4, setChoice4] = useState('');
+    const [formState, setFormState] = useState({ question: '', correct: '', choice2: '', choice3: '', choice4: '' });
     const [questionNumber, setQuestionNumber] = useState(1);
     const [questions, setQuestions] = useState([]);
 
     const addQuestion = (e) => {
-        e.preventDefault();
-
-        if(question === '') {
-            alert('A question is required!');
-            return;
-        }
-
-        if(correct === '') {
-            alert('A correct choice is required!');
-            return;
-        }
-
-        if(choice2 === '' && choice3 === '' && choice4 === '') {
-            alert('At least one other choice is required!');
-            return;
-        }
-
-        if(questionNumber === numOfQuestions + 1) {
-            alert(`The maximum number of questions allowed is ${numOfQuestions}!`);
-            return;
-        }
-        
-        let questionObj = {
-            question: question,
-            correct: correct,
-            choice2: choice2,
-            choice3: choice3,
-            choice4: choice4
-        };
-
-        if(questions.length >= 1) {
-            if(questionObj.question.trim() === questions[questions.length - 1].question.trim()) {
-                alert('This question is the same as the previous question.');
-                return;
-            }
-        }
-
-        setQuestions([...questions, questionObj]);
-        setQuestionNumber(questionNumber + 1);
-        setTextFieldsBlank();
+       processQuestion(e, numOfQuestions, formState, questions, questionNumber, setQuestions, setQuestionNumber)
     }
 
     let navigate = useNavigate();
 
     const handleSubmit = (e) => {
-        e.preventDefault();
-
-        if(questionNumber < 10) {
-            alert('Every test must have at least 10 questions!');
-            return;
-        }
-
-        const id = user.id;
-
-        if(id) {
-            axios.post(`${baseURL}/api/questions/${testId}`, {
-                questions: questions
-            })
-            .then(response => {
-                console.log(response);
-                alert(response.data.msg);
-                navigate('/');
-            })
-            .catch(error => {
-                console.log(error);
-            });
-        }
+       processSubmission(e, user, testId, questions, questionNumber, navigate);
     }
 
     return (
@@ -100,27 +30,54 @@ const Questions = ({ user, testId, numOfQuestions }) => {
                     <br/>
                     <div>
                         <label htmlFor='question' id='question-heading' style={{ fontWeight: 'bold' }}>Question {questionNumber}</label><br/>
-                        <input id='question' type='text' name='question' onChange={e => setQuestion(e.target.value)}></input>
+                        <input 
+                            id='question' 
+                            type='text' 
+                            name='question' 
+                            onChange={e => setFormState({ ...formState, [e.target.name]: e.target.value })}
+                        >
+                        </input>
                     </div>
                     <br/>
                     <div>
                         <label htmlFor='correct-answer'>Correct Answer</label><br/>
-                        <input id='choice-1' type='text' name='correct-answer' onChange={e => setCorrect(e.target.value)}></input>
+                        <input 
+                            id='choice-1' 
+                            type='text' 
+                            name='correct' 
+                            onChange={e => setFormState({ ...formState, [e.target.name]: e.target.value })}
+                        >
+                        </input>
                     </div>
                     <br/>
                     <div>
                         <label htmlFor='second-choice'>Choice</label><br/>
-                        <input id='choice-2' type='text' name='second-choice' onChange={e => setChoice2(e.target.value)}></input>
+                        <input 
+                            id='choice-2' 
+                            type='text' 
+                            name='choice2' 
+                            onChange={e => setFormState({ ...formState, [e.target.name]: e.target.value })}
+                        ></input>
                     </div>
                     <br/>
                     <div>
                         <label htmlFor='third-choice'>Choice</label><br/>
-                        <input id='choice-3' type='text' name='third-choice' onChange={e => setChoice3(e.target.value)}></input>
+                        <input 
+                            id='choice-3' 
+                            type='text' 
+                            name='choice3' 
+                            onChange={e => setFormState({ ...formState, [e.target.name]: e.target.value })}
+                        ></input>
                     </div>
                     <br/>
                     <div>
                         <label htmlFor='fourth-choice'>Choice</label><br/>
-                        <input id='choice-4' type='text' name='fourth-choice' onChange={e => setChoice4(e.target.value)}></input>
+                        <input 
+                            id='choice-4' 
+                            type='text' 
+                            name='choice4' 
+                            onChange={e => setFormState({ ...formState, [e.target.name]: e.target.value })}
+                        ></input>
                     </div>
                     <br/>
                     <button id='question-add' onClick={addQuestion}>Add</button>

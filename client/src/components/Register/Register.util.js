15086edb36efb8ca249108cbfe543525a15c17d0
CoderJ01 <buildingguy@sbcglobal.net>
2023-12-09 15:27:33 -0800

    creation function to process registration

---
+++ client/src/components/Register/Register.util.js
@@ -0,0 +1,23 @@
+import { postInfo } from '../../utils/requests';
+import isValidEmail from '../../utils/emailValidation';
+
+export const processSubmission = (e, form, navigate) => {
+    e.preventDefault();
+
+    if(form.username === '' || form.email === '' || form.password === '') {
+        alert('All fields need to be filled in!');
+        return;
+    }
+
+    if(!isValidEmail(form.email)) {
+        alert('Email is invalid!');
+        return;
+    }
+
+    if(form.password.length < 8) {
+        alert('Password needs to be at least 8 characters!');
+        return;
+    }
+
+    postInfo('api/users/register', form, navigate);
+}

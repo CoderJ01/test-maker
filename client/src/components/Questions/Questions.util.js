13f260a1806c45c41ba5ae6504d1d3b9b89017db
CoderJ01 <buildingguy@sbcglobal.net>
2023-12-09 15:11:10 -0800

    create function to process questions

--- client/src/components/Questions/Questions.util.js
+++ client/src/components/Questions/Questions.util.js
@@ -1,4 +1,49 @@
-export const setTextFieldsBlank = () => {
+import { postInfo } from "../../utils/requests";
+
+export const processQuestion = (e, numOfQuestions, form, questions, questionNumber, setQuestions, setQuestionNumber) => {
+    e.preventDefault();
+
+    if(form.question === '') {
+        alert('A question is required!');
+        return;
+    }
+
+    if(form.correct === '') {
+        alert('A correct choice is required!');
+        return;
+    }
+
+    if(form.choice2 === '' && form.choice3 === '' && form.choice4 === '') {
+        alert('At least one other choice is required!');
+        return;
+    }
+
+    if(questionNumber === numOfQuestions + 1) {
+        alert(`The maximum number of questions allowed is ${numOfQuestions}!`);
+        return;
+    }
+    
+    let questionObj = {
+        question: form.question,
+        correct: form.correct,
+        choice2: form.choice2,
+        choice3: form.choice3,
+        choice4: form.choice4
+    };
+
+    if(questions.length >= 1) {
+        if(questionObj.question.trim() === questions[questions.length - 1].question.trim()) {
+            alert('This question is the same as the previous question.');
+            return;
+        }
+    }
+
+    setQuestions([...questions, questionObj]);
+    setQuestionNumber(questionNumber + 1);
+    setTextFieldsBlank();
+}
+
+const setTextFieldsBlank = () => {
     let field = [];
     
     // looping proves to be ineffective for this type of task
@@ -13,4 +58,17 @@ export const setTextFieldsBlank = () => {
     field[2].value = '';
     field[3].value = '';
     field[4].value = '';
+}
+
+export const processSubmission = (e, user, testId, questions, questionNumber, navigate) => {
+    e.preventDefault();
+
+    if(questionNumber < 10) {
+        alert('Every test must have at least 10 questions!');
+        return;
+    }
+
+    if(user.id) {
+        postInfo(`api/questions/${testId}`, questions, navigate);
+    }
 }

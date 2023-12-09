5fea0c9717dd59173a93b7dea8f540e4a5e318a4
CoderJ01 <buildingguy@sbcglobal.net>
2023-12-09 15:56:47 -0800

    use custom hook to retrieve test scores

--- client/src/components/Scores/Scores.component.jsx
+++ client/src/components/Scores/Scores.component.jsx
@@ -1,46 +1,21 @@
-// React
-import React, { useState, useEffect, useCallback } from 'react';
-
 // CSS
 import './Scores.style.css';
 
-// URL
-import { baseURL } from '../../utils/urls';
-
-// other imports
-import axios from 'axios';
+// utils
+import { GetData } from '../../utils/requests';
 
 const Scores = ({ user }) => {
-    const [scores, setScores] = useState([]);
-    const [tests, setTests] = useState([]);
-
-    const fetchScores = useCallback(async () => {
-        const id = user.id;
-
-        if(id) {
-            try {
-                const response = await axios.get(`${baseURL}/api/scores/${id}`);
-                setScores(response.data.scores);
-                setTests(response.data.tests);
-            }
-            catch(error) {
-                console.log(error);
-            }
-        }
-    }, [user.id]);
-
-    useEffect(() => {
-        fetchScores();
-    }, [fetchScores]);
-
+    const scores = GetData(`api/scores/${user.id}`);
+    const tests = GetData(`api/scores/${user.id}`);
+    
     return (
         <div className='scores'>
             <h2>Scores</h2>
             {
-                scores.map((score , i) => {
+                scores?.data?.scores?.map((score , i) => {
                     return (
                         <div className='scores-single-score'>
-                            <p><span style={{ fontWeight: 'bold' }}>Test: </span>{tests[i]?.title}</p>
+                            <p><span style={{ fontWeight: 'bold' }}>Test: </span>{tests?.data?.tests[i].title}</p>
                             <p><span style={{ fontWeight: 'bold' }}>Score: </span>{(score?.score) * 100}%</p>
                         </div>
                     );

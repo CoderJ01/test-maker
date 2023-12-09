3a95ee01bee1ef81a2651b5c87f1333acde52444
CoderJ01 <buildingguy@sbcglobal.net>
2023-12-09 14:27:38 -0800

    fetch number of questions using custom hook

--- client/src/components/NumberOfQuestions/NumberOfQuestions.jsx
+++ client/src/components/NumberOfQuestions/NumberOfQuestions.jsx
@@ -1,40 +1,14 @@
-// React
-import React, { useState, useEffect, useCallback } from 'react';
-
 // CSS
 import './NumberOfQuestions.css';
 
-// URL
-import { baseURL } from '../../utils/urls';
-
-// other imports
-import axios from 'axios';
+// utils
+import { GetData } from '../../utils/requests';
 
 const NumberOfQuestions = ({ testId }) => {
-    const [questions, setQuestions] = useState();
-
-    const fetchQuestions = useCallback(async () => {
-        const id = testId;
-
-        if(id) {
-            if(id) {
-                try {
-                    const response = await axios.get(`${baseURL}/api/questions/${id}`);
-                    setQuestions(response.data);
-                }
-                catch(error) {
-                    console.log(error);
-                }
-            }
-        }
-    }, [testId]);
-
-    useEffect(() => {
-        fetchQuestions();
-    }, [fetchQuestions]);
+    const questions = GetData(`api/questions/${testId}`);
 
     return (
-        <p id='tst-questions'>Number of Questions: {questions?.length}</p>
+        <p id='tst-questions'>Number of Questions: {questions?.data?.length}</p>
     )
 }
 

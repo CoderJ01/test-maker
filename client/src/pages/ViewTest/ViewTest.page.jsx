58691533514fa41d4ec64b4e28f3ae2b816b0a00
CoderJ01 <buildingguy@sbcglobal.net>
2023-12-09 18:51:33 -0800

    custom hooks

--- client/src/pages/ViewTest/ViewTest.page.jsx
+++ client/src/pages/ViewTest/ViewTest.page.jsx
@@ -1,58 +1,17 @@
 // React
-import React, { useState, useEffect, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
 
 // CSS 
 import './ViewTest.style.css';
 
-// URL
-import { baseURL } from '../../utils/urls';
-
-// other imports
-import axios from 'axios';
+// util
+import { GetData } from '../../utils/requests';
 
 const ViewTest = ({ user }) => {
     const { userId, testId } = useParams();
 
-    const [test, setTest] = useState([]);
-    const [questions, setQuestions] = useState([]);
-
-    const fetchTest = useCallback(async () => {
-        const id = testId;
-
-        if(id) {
-            try {
-                const response = await axios.get(`${baseURL}/api/tests/single-test/${id}`);
-                setTest(response.data);
-            }
-            catch(error) {
-                console.log(error);
-            }
-        }
-    }, [testId]);
-
-    useEffect(() => {
-        fetchTest();
-    }, [fetchTest]);
-
-    const fetchQuestions = useCallback(async () => {
-        const id = testId;
-
-        if(id) {
-            try {
-                const response = await axios.get(`${baseURL}/api/questions/${id}`);
-                setQuestions(response.data)
-
-            }
-            catch(error) {
-                console.log(error);
-            }
-        }
-    }, [testId]);
-
-    useEffect(() => {
-        fetchQuestions();
-    }, [fetchQuestions]);
+    const test = GetData(`api/tests/single-test/${testId}`);
+    const questions = GetData(`api/questions/${testId}`);
 
     if(user.id !== userId) {
         return (
@@ -65,10 +24,10 @@ const ViewTest = ({ user }) => {
 
     return (
         <div className='view-test'>
-            <h2>{test.title}</h2>
-            <p id='vt-description'>{test.description}</p>
+            <h2>{test?.data?.title}</h2>
+            <p id='vt-description'>{test?.data?.description}</p>
             {
-                questions.length === 0 ? 
+                questions?.data?.length === 0 ? 
                 (
                     <>
                         <br/>
@@ -78,7 +37,7 @@ const ViewTest = ({ user }) => {
                 (
                     <div className='view-test-questions'>
                     {
-                        questions.map(question => {
+                        questions?.data?.map(question => {
                             return (
                                 <div className='vtq-question'>
                                     <h3>{question.question_header}</h3>

93720486c21fae7913297c3692fecc276d48472e
CoderJ01 <buildingguy@sbcglobal.net>
2023-12-09 22:11:54 -0800

    shuffle GET response (questions) directly to limit re-rendering

--- client/src/utils/requests.js
+++ client/src/utils/requests.js
@@ -11,13 +11,14 @@ import cookie from 'js-cookie';
 const baseURL_server = process.env.REACT_APP_SERVER_URL;
 
 // GET
-export function GetData(route, user = false) {
+export function GetData(route, user = false, questions = false) {
     const [data, setData] = useState([]);
 
     const getInfo = useCallback(async() => {
         try {
             const response = await axios.get(`${baseURL_server}/${route}`);
             if(user) getUser(response, setData);
+            else if(questions) randomizeChoices(modifyQuestions(response?.data), setData);
             else setData(response);
         }
         catch(error) {
@@ -39,6 +40,46 @@ function getUser(response, setData) {
             setData(response.data[i]);
         }
     }
+}
+
+function modifyQuestions(questions) {
+    let modifiedQuestions = [];
+
+    // put answer choices per question in the same array
+    for(let i = 0; i < questions?.length; i++) {
+        modifiedQuestions[i] = {
+            question: questions[i]?.question_header,
+            choices: [
+                questions[i]?.correct_answer,
+                questions[i]?.second_choice,
+                questions[i]?.third_choice,
+                questions[i]?.fourth_choice
+            ]
+        }
+    }
+
+    return modifiedQuestions;
+}
+
+function randomizeChoices(questions, setData) {
+    for(let i = 0; i < questions?.length; i++) {
+        shuffleAnswerChoices(questions[i]?.choices);
+    }
+    setData(questions);
+}
+
+function shuffleAnswerChoices(array) {
+    let currentIndex = array?.length,  randomIndex;
+
+    while (currentIndex !== 0) {
+
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [array[currentIndex], array[randomIndex]] = 
+        [array[randomIndex], array[currentIndex]];
+    }
+    return array;
 }
 
 // POST

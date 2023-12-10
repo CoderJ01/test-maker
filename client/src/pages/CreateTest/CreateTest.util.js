c0941644b3985929392599b5c14a5f75ae1aa9de
CoderJ01 <buildingguy@sbcglobal.net>
2023-12-09 16:39:17 -0800

    create function to process creation of test

--- client/src/pages/CreateTest/CreateTest.util.js
+++ client/src/pages/CreateTest/CreateTest.util.js
@@ -1,3 +1,5 @@
+import { postInfo } from "../../utils/requests";
+
 export const listOptions = () => {
     let options = [];
 
@@ -6,4 +8,22 @@ export const listOptions = () => {
     }
 
     return options;
+}
+
+export const processSubmission = (e, form, user) => {
+    e.preventDefault();
+
+    if(form.title === '') {
+        alert('The test needs a title!');
+        return;
+    }
+
+    if(form.description === '') {
+        alert('The test needs a description!');
+        return;
+    }
+
+    if(user.id) {
+        postInfo(`api/tests/${user.id}`, form, '');
+    }
 }

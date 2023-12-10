4841cdf4616376a90fd18a65578807b14e7d8656
CoderJ01 <buildingguy@sbcglobal.net>
2023-12-09 22:13:14 -0800

    clean

--- client/src/pages/TakeTest/TakeTest.util.js
+++ client/src/pages/TakeTest/TakeTest.util.js
@@ -1,44 +1,6 @@
 // util 
 import { postInfo } from '../../utils/requests';
 
-export const modifyQuestions = (questions) => {
-    let modifiedQuestions = [];
-
-    // put answer choices per question in the same array
-    for(let i = 0; i < questions?.length; i++) {
-        modifiedQuestions[i] = {
-            question: questions[i]?.question_header,
-            choices: [
-                questions[i]?.correct_answer,
-                questions[i]?.second_choice,
-                questions[i]?.third_choice,
-                questions[i]?.fourth_choice
-            ]
-        }
-    }
-
-    return modifiedQuestions;
-}
-
-export const randomizeChoices = (questions) => {
-    for(let i = 0; i < questions?.length; i++) {
-        shuffleAnswerChoices(questions[i]?.choices);
-    }
-}
-
-const shuffleAnswerChoices = (array) => {
-    let currentIndex = array.length,  randomIndex;
-
-    while (currentIndex !== 0) {
-
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-
-        [array[currentIndex], array[randomIndex]] = 
-        [array[randomIndex], array[currentIndex]];
-    }
-}
-
 // for each question, ensure that no radio buttons are pre-selected 
 // before the test taker makes a selection 
 export const setRadioButtonBlank = () => {

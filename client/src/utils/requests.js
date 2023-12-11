f7ad69740f9adecbb5e08b3880ab65fe630aa727
CoderJ01 <buildingguy@sbcglobal.net>
2023-12-11 14:13:08 -0800

    import/implement

--- client/src/utils/requests.js
+++ client/src/utils/requests.js
@@ -4,6 +4,9 @@ import { useState, useEffect, useCallback } from 'react';
 // setCookie
 import setCookie from './setCookie';
 
+// modifyQuestions
+import { modifyQuestions, randomizeChoices } from './modifyQuestions';
+
 // other imports
 import axios from 'axios';
 import cookie from 'js-cookie';
@@ -39,45 +42,6 @@ function getUser(response, setData) {
         if(response.data[i].random_string === userCookie) {
             setData(response.data[i]);
         }
-    }
-}
-
-function modifyQuestions(questions) {
-    let modifiedQuestions = [];
-
-    // put answer choices per question in the same array
-    for(let i = 0; i < questions?.length; i++) {
-        modifiedQuestions[i] = {
-            question: questions[i]?.question_header,
-            choices: [
-                questions[i]?.correct_answer,
-                questions[i]?.second_choice,
-                questions[i]?.third_choice,
-                questions[i]?.fourth_choice
-            ]
-        }
-    }
-
-    return modifiedQuestions;
-}
-
-function randomizeChoices(questions, setData) {
-    for(let i = 0; i < questions?.length; i++) {
-        shuffleAnswerChoices(questions[i]?.choices);
-    }
-    setData(questions);
-}
-
-function shuffleAnswerChoices(array) {
-    let currentIndex = array?.length,  randomIndex;
-
-    while (currentIndex !== 0) {
-
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-
-        [array[currentIndex], array[randomIndex]] = 
-        [array[randomIndex], array[currentIndex]];
     }
 }
 
